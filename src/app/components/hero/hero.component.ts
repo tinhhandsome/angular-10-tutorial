b4a4f148014d781f65c0034abe0a6f2cd08c9b1a
Nguyễn Tịnh <43327850+1660623@users.noreply.github.com>
2020-08-08 00:18:26 +0700

    add selected 1 user

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -12,16 +12,20 @@ export class HeroComponent implements OnInit {
   selectedHero: Hero
   heroes: Hero[]
 
-  constructor( private heroService: HeroService, private MessageService: MessageService) { }
+  constructor( private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes()
-    console.log(`${this.heroes[0].name}`)
+    // console.log(`${this.heroes[0].name}`)
   }
 
   // onSubmit(){
   //   this.MessageService.addMessage()
   // }
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
+  }
   getHeroes(): void {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes)
